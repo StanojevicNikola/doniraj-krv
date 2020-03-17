02a15a42a42ca89ea834e876c7af867254159d82
StanojevicNikola <stanojevic.business@gmail.com>
2020-03-17 23:03:27 +0100

    added server, google auth and cookie

---
+++ server/config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+    googleClientID:
+        "171718459252-69u111eh9fen9305qhcn0omn91f45t3d.apps.googleusercontent.com",
+    googleClientSecret: "qMGgZ5aWp77Hd4d9aDzyaXNf",
+    mongoURI:
+        "mongodb+srv://nikola:S1qjkNODdQLZtk4o@donirajkrv-adtiy.mongodb.net/test?retryWrites=true&w=majority",
+    cookieKey: "redacted"
+};
---
+++ server/services/passport.js
@@ -0,0 +1,35 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+const keys = require("../config/keys");
+
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+    const user = await User.findById(id);
+    done(null, user);
+});
+
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: "/auth/google/callback",
+            proxy: true
+        },
+        async (accessToken, refreshToken, profile, done) => {
+            const existingUser = await User.findOne({ googleID: profile.id });
+            // we already have a record with the given profile ID
+            if (existingUser) return done(null, existingUser);
+
+            // we don't have a user record with this ID, make a new record
+            const user = await new User({ googleID: profile.id }).save();
+            done(null, user);
+        }
+    )
+);
