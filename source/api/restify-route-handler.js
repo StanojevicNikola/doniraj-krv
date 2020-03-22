abd110c2d75d9f73eced20d74005133b4145540f
antic11d <dimitrije.misa@gmail.com>
2020-03-22 17:59:28 +0100

    Added get info routes

--- source/api/restify-route-handler.js
+++ source/api/restify-route-handler.js
@@ -1,35 +1,55 @@
+const utils = require('../utils');
+
 class RestifyRouteHandler {
-    constructor({ logger, placeController, requestController }) {
+    constructor({
+        logger, placeController, requestController, geolocationService, bloodGroupService,
+    }) {
         this.logger = logger;
         this.placeController = placeController;
         this.requestController = requestController;
+        this.geolocationService = geolocationService;
+        this.bloodGroupService = bloodGroupService;
     }
 
 
     async hello(req, res, next) {
         this.logger.info('Hello');
-        this.sendSuccess(res, 'Success', { data: 'caocao' });
+        this._sendSuccess(res, 'Success', { data: 'caocao' });
         next();
     }
 
     async findPlaces(req, res, next) {
         this.logger.info('findPlaces');
         const data = await this.placeController.find(req);
-        if (data.length > 0) this.sendSuccess(res, 'Success', data);
+        if (data.length > 0) this._sendSuccess(res, 'Success', data);
         else {
-            this.sendSuccess(res, 'Nema lokacija u trazenoj okolini!', {});
+            this._sendSuccess(res, 'Nema lokacija u trazenoj okolini!', {});
         }
         next();
     }
 
     async requestBlood(req, res, next) {
-        this.logger.info('blood request triggered');
+        this.logger.info('requestBlood');
         const donors = await this.requestController.publishRequest(req.body);
-        this.sendSuccess(res, 'Mailovi su poslati na adrese', { donors });
+        this._sendSuccess(res, 'Mailovi su poslati na adrese', { donors });
         next();
     }
 
-    sendResponse(res, status, responseData) {
+    async getCities(req, res, next) {
+        this.logger.info('getCities');
+        const data = await this.geolocationService.find({});
+        this._sendSuccess(res, 'Success', data);
+        next();
+    }
+
+    async getBloodGroups(req, res, next) {
+        this.logger.info('getBloodGroups');
+        const data = await this.bloodGroupService.find({});
+        this._sendSuccess(res, 'Success', utils.extract(data, 'groupType'));
+        next();
+    }
+
+    _sendResponse(res, status, responseData) {
         res.status(status);
         res.send({
             ...responseData,
@@ -38,16 +58,16 @@ class RestifyRouteHandler {
         });
     }
 
-    sendSuccess(res, message, data) {
-        this.sendResponse(res, 200, { message, data });
+    _sendSuccess(res, message, data) {
+        this._sendResponse(res, 200, { message, data });
     }
 
-    sendBadRequest(res, message, data) {
-        this.sendResponse(res, 400, { message, data });
+    _sendBadRequest(res, message, data) {
+        this._sendResponse(res, 400, { message, data });
     }
 
-    sendUnauthorized(res, message, data) {
-        this.sendResponse(res, 401, { message, data });
+    _sendUnauthorized(res, message, data) {
+        this._sendResponse(res, 401, { message, data });
     }
 }
 
