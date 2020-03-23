aa62d63ec2763d42633c1193036aeddf6d46f0f2
akinovak <akinovak@gmail.com>
2020-03-23 13:05:44 +0100

    minor fix

--- ids/ids.js
+++ ids/ids.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
 
-module.exports.user1Id = new mongoose.Types.ObjectId();
+module.exports.user1Id = mongoose.Types.ObjectId();
 module.exports.donor1Id = mongoose.Types.ObjectId();
