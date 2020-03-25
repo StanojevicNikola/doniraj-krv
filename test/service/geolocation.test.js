1a0b0f23eb7ca2c9ffcb92ab7cc79bd5b1db1dee
akinovak <akinovak@gmail.com>
2020-03-25 14:00:30 +0100

    test bootstrap

---
+++ test/service/geolocation.test.js
@@ -0,0 +1,41 @@
+const {
+    describe, it, beforeEach, afterEach,
+} = require('mocha');
+
+const assert = require('assert');
+const mongoose = require('mongoose');
+
+const app = require('../test-app');
+
+describe('Create tag creation claim use case', () => {
+    beforeEach(async () => {
+        const storage = app.container.resolve('storage');
+        await storage.connect();
+    });
+
+    it('Should create tag creation claim object in database', async () => {
+        try {
+            const geolocationService = await app.container.resolve('geolocationService');
+            const geolocation = {
+                city: 'Jagodina',
+                lat: '123',
+                lng: '233',
+            };
+            const geoId = await geolocationService.create(geolocation);
+            const fetchedGeolocation = await geolocationService.findById(geoId);
+            geolocation._id = geoId;
+            console.log(fetchedGeolocation);
+            assert.deepEqual(geoId, fetchedGeolocation._id, 'Fetched geolocation id should be same as inserted one');
+            assert.equal(geolocation.lat, fetchedGeolocation.lat, 'Fetched geolocation lat should be same as inserted one');
+            assert.equal(geolocation.lng, fetchedGeolocation.lng, 'Fetched geolocation lng should be same as inserted one');
+        } catch (err) {
+            assert(false, err);
+        }
+    });
+
+    afterEach(async () => {
+        const storage = app.container.resolve('storage');
+        await storage.drop();
+        await storage.disconnect();
+    });
+});
---
+++ test/test-app.js
@@ -0,0 +1,4 @@
+const App = require('../source/app');
+const config = require('../config');
+
+module.exports = new App(config);
