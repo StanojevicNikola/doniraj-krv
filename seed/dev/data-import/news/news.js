bdb68bf5cd369dff9a01fbe091cc2fa69f3bdf5f
Dimitrije Antić <dimitrije.misa@gmail.com>
2020-03-29 14:57:54 +0000

    Update news.js

--- seed/dev/data-import/news/news.js
+++ seed/dev/data-import/news/news.js
@@ -1,5 +1,5 @@
 const ids = require('../../ids/ids');
-const getTime = require('../time-converter');
+const getDate = require('../time-converter');
 
 const offset = '-';
 
@@ -8,24 +8,24 @@ module.exports = [
         _id: ids.news1,
         title: 'Trazi se donor u Beogradu!',
         description: 'Potrebna A+ krvna grupa. Medicinski Centar Banjica. Broj jedinica: 2.',
-        date: getTime(0, offset),
+        date: getDate(0, offset),
     },
     {
         _id: ids.news2,
         title: 'Trazi se donor u Novom Sadu!',
         description: 'Potrebna A- krvna grupa. Institut za pulmologiju. Broj jedinica: 1.',
-        date: getTime(7, offset),
+        date: getDate(7, offset),
     },
     {
         _id: ids.news3,
         title: 'Trazi se donor u Cacku! Hitno!',
         description: 'Potrebna AB+ krvna grupa. Gradska bolnica. Broj jedinica: 5.',
-        date: getTime(15, offset),
+        date: getDate(15, offset),
     },
     {
         _id: ids.news4,
         title: 'Trazi se donor u Smederevu!',
         description: 'Potrebna O+ krvna grupa. Gradski zavod za transfuziju. Broj jedinica: 3.',
-        date: getTime(30, offset),
+        date: getDate(30, offset),
     },
 ];
